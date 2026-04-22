a87cca9cbdf9f5e93a228709d0054765d592a808
Chitranjan Gupta <chitranjangupta40@gmail.com>
2026-04-22 17:09:00 +0530

    Create App component for Paradise Nursery

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Paradise Nursery</h1>
+      <p>Welcome to our plant shop 🌿</p>
+      <button style={{ padding: "10px 20px", fontSize: "16px" }}>
+        Get Started
+      </button>
+    </div>
+  );
+}
+
+export default App;
