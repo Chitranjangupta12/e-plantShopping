c9ab8ed6c95ad51b8053c47f8b55a1d7fbe66187
Chitranjan Gupta <94cse2324@rkgit.edu.in>
2026-04-22 17:05:12 +0530

    Add AboutUs component for nursery information

---
+++ AboutUs.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+function AboutUs() {
+  return (
+    <div>
+      <h1>About Paradise Nursery</h1>
+      <p>
+        Paradise Nursery offers a wide variety of indoor plants to decorate your home.
+      </p>
+    </div>
+  );
+}
+
+export default AboutUs;
